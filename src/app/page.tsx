3f421b20ad5bfec08e162a49d9d32fa9b62b934a
rayofsunshine <rtao1014@gmail.com>
2025-03-15 23:05:42 -0700

    Add NavBar component and integrate it into the Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import NavBar from "@/components/NavBar";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="text-white flex flex-col h-screen">
-      <div className="bg-neutral-800">Header</div>
+      <NavBar></NavBar>
       <div className="bg-neutral-600 flex-1">Conten</div>
     </div>
   );
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="flex items-center gap-2 text-black">
+      <img src="/logo.webp" alt="Logo" className="w-12" />
+      Navbar
+    </div>
+  );
+};
+
+export default NavBar;
