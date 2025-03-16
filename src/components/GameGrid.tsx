49c0d8b24db85933fe6072cb0813969cf4be1cad
rayofsunshine <rtao1014@gmail.com>
2025-03-16 14:17:08 -0700

    Fetch games from API and display in GameGrid component; update API client to use NEXT_PUBLIC_RAWG_API_KEY

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,11 +1,40 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import apiClient from "@/services/api-client";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
 
 export const GameGrid = () => {
-  //
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    apiClient
+      .get<FetchGamesResponse>("/games")
+      .then((response) => {
+        setGames(response.data.results);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  }, []);
 
   return (
     <div>
-      <ul></ul>
+      <ul>
+        {games.map((e) => (
+          <li key={e.id}>{e.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/services/api-client.tsx
+++ src/services/api-client.tsx
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+if (!process.env.NEXT_PUBLIC_RAWG_API_KEY) {
+  throw new Error("RAWG_API_KEY is not defined");
+}
+
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: process.env.RAWG_API_KEY,
+    key: process.env.NEXT_PUBLIC_RAWG_API_KEY,
   },
 });
