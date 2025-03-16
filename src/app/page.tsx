40c5730c5b1d37481689ddc251b3f5706d589f3d
rayofsunshine <rtao1014@gmail.com>
2025-03-15 22:50:21 -0700

    Update layout in Home component with header and content sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-      <Button variant={"outline"}>Hello</Button>
+    <div className="text-white flex flex-col h-screen">
+      <div className="bg-neutral-800">Header</div>
+      <div className="bg-neutral-600 flex-1">Conten</div>
     </div>
   );
 }
