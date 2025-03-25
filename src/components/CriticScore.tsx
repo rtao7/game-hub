d0df3090316c2f2e8dba0b97ac5da38cf2c44539
rayofsunshine <rtao1014@gmail.com>
2025-03-24 18:18:15 -0700

    Adjust CriticScore component to use smaller text size for score display

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -9,5 +9,5 @@ export const CriticScore = ({ score }: CriticScoreProp) => {
   let color =
     score > 80 ? "bg-green-600" : score > 60 ? "bg-yellow-600" : "bg-red-600";
 
-  return <Badge className={`${color} text-xl`}>{score}</Badge>;
+  return <Badge className={`${color} text-sm`}>{score}</Badge>;
 };
