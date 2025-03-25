a0b4702a84fc91481ba3efdccb1277654fb7603a
rayofsunshine <rtao1014@gmail.com>
2025-03-24 21:44:49 -0700

    get optimized images.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, CardContent, CardFooter, CardHeader } from "./ui/card";
 import { useGames, Game } from "@/hooks/useGames";
 import { CriticScore } from "./CriticScore";
+import getCroppedImageUrl from "@/services/image-url";
 
 interface GameCardProp {
   game: Game;
@@ -12,7 +13,11 @@ export const GameCard = ({ game }: GameCardProp) => {
     <div>
       <Card className="gap-2 w-96 h-fit text-black rounded-sm p-0 overflow-hidden">
         <CardContent className="p-0">
-          <img src={game.background_image} alt={game.name} className="h-64" />
+          <img
+            src={getCroppedImageUrl(game.background_image)}
+            alt={game.name}
+            className="h-64"
+          />
         </CardContent>
         <CardHeader className="px-3 font-medium">
           {game.name} <CriticScore score={game.metacritic} />
