1ad6e956aeee36a60bf180573351e5df23eb84a7
rayofsunshine <rtao1014@gmail.com>
2025-03-25 22:22:25 -0700

    Update GameGrid to display multiple skeleton components during loading state for better UX

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,11 +5,12 @@ import { GameCardSkeleton } from "./GameCardSkeleton";
 
 export const GameGrid = () => {
   const { games, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <div className="columns-3 m-6 gap-6">
       {error && <p className="text-black">{error}</p>}
-      {isLoading && <GameCardSkeleton />}
+      {isLoading && skeletons.map((e) => <GameCardSkeleton key={e} />)}
       {games.map((game) => (
         <GameCard key={game.id} game={game} />
       ))}
