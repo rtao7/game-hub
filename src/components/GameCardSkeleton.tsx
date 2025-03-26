72e5fe6a772840650cbcb1c15d0db77512e130ec
rayofsunshine <rtao1014@gmail.com>
2025-03-25 22:25:15 -0700

    Refactor GameCardSkeleton layout for improved styling and spacing

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "./ui/skeleton";
 
 export const GameCardSkeleton = () => {
   return (
-    <div className="flex items-center space-x-4">
+    <div className="gap-2 w-96 h-96 text-black rounded-sm p-0 overflow-hidden border">
       <Skeleton className="h-12 w-12 rounded-full" />
       <div className="space-y-2">
         <Skeleton className="h-4 w-[250px]" />
